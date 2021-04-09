2cde1b71679ad433e62d270ef6713171e29a3b04
Mirlan Ipasov <mipasov@gmail.com>
2021-04-10 00:38:26 +0600

    initial project setup

---
+++ src/main/java/com/binarycod/arigato/ArigatoApplication.java
@@ -0,0 +1,13 @@
+package com.binarycod.arigato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArigatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArigatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binarycod/arigato/ArigatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binarycod.arigato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArigatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
