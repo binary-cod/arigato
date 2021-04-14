21829ea66a3add3c6d73428da715b620863b530d
Mirlan Ipasov <mipasov@gmail.com>
2021-04-14 09:04:40 +0600

    Get and Post handlers for /products.

--- src/main/java/com/binarycod/arigato/controllers/ProductsController.java
+++ src/main/java/com/binarycod/arigato/controllers/ProductsController.java
@@ -1,13 +1,42 @@
 package com.binarycod.arigato.controllers;
 
+import com.binarycod.arigato.domain.Product;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.ArrayList;
+
 
 @Controller
+@RequestMapping("/products")
 public class ProductsController {
 
-    @GetMapping("/products")
-    public String getProducts(){
+    List<Product> productList = new ArrayList<>();
+
+    @GetMapping
+    public String getProducts(Model model){
+        model.addAttribute("products", productList);
+        model.addAttribute("product", new Product());
         return "product_list";
     }
+
+    @PostMapping
+    public String createProduct(@RequestParam Long pid, @RequestParam String pname, @RequestParam Double price, @RequestParam Integer size){
+        System.out.println("I am handling post to this endpoint!");
+        System.out.println(pid +"  "+ pname + "  "+price);
+        productList.add(new Product(pid, pname, price));
+        return "redirect:/products";
+    }
+
+    @GetMapping("/delete")
+    public String deleteProduct(@RequestParam Long id){
+
+        return "redirect:/products";
+    }
+
 }
---
+++ src/main/java/com/binarycod/arigato/domain/Product.java
@@ -0,0 +1,41 @@
+package com.binarycod.arigato.domain;
+
+public class Product {
+
+    Long id;
+    String name;
+    Double price;
+
+    public Product(){
+    }
+
+    public Product(Long id, String name, Double price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public java.lang.Long getId() {
+        return id;
+    }
+
+    public void setId(java.lang.Long id) {
+        this.id = id;
+    }
+
+    public java.lang.String getName() {
+        return name;
+    }
+
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+
+    public java.lang.Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(java.lang.Double price) {
+        this.price = price;
+    }
+}
