aedb927c5296132574c399e0ff02dc39d33f293e
Guli555 <uguli4177@gmail.com>
2021-05-20 13:36:14 -0500

    Validating that ProductService is not empty

--- src/test/java/com/binarycod/arigato/ArigatoApplicationTests.java
+++ src/test/java/com/binarycod/arigato/ArigatoApplicationTests.java
@@ -1,13 +1,28 @@
 package com.binarycod.arigato;
 
+import com.binarycod.arigato.services.ProductService;
+import javafx.beans.binding.Bindings;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class ArigatoApplicationTests {
 
+	@Autowired
+	ProductService productService;
+	
+
 	@Test
-	void contextLoads() {
+	 void contextLoads() throws Exception {
+		assertThat(productService).isNotNull();
+
+
+
 	}
 
 }
