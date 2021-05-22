f0f30d0ccc55b01509c015c32c8126b6a824de4c
Mirlan Ipasov <mipasov@gmail.com>
2021-05-23 00:27:17 +0600

    product list pages and product details pages customized to have some look and feel.

--- src/main/java/com/binarycod/arigato/controllers/HomeController.java
+++ src/main/java/com/binarycod/arigato/controllers/HomeController.java
@@ -1,10 +1,14 @@
 package com.binarycod.arigato.controllers;
 
+import com.binarycod.arigato.domain.Product;
 import com.binarycod.arigato.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class HomeController {
@@ -16,6 +20,16 @@ public class HomeController {
     public String getHome(Model model){
         model.addAttribute("products", productService.getAllProducts());
         return "home";
+    }
+
+    @GetMapping("/public/product/details")
+    public String showProductDetails(@RequestParam Long id, Model model){
+        Optional<Product> optionalProduct = productService.getProductById(id);
+        if (!optionalProduct.isPresent())
+            return "redirect:/";
+
+        model.addAttribute("product", optionalProduct.get());
+        return "product_details";
     }
 
 }
