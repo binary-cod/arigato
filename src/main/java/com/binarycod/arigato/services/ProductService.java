93ea6f374fb64e1c7e266fe0015d020c4078dc4d
gantu <mipasov@gmail.com>
2021-06-12 23:58:13 +0600

    pagination experience implemented for products page. UI can control load of the products.

--- src/main/java/com/binarycod/arigato/services/ProductService.java
+++ src/main/java/com/binarycod/arigato/services/ProductService.java
@@ -3,6 +3,9 @@ package com.binarycod.arigato.services;
 import com.binarycod.arigato.domain.Product;
 import com.binarycod.arigato.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -31,4 +34,10 @@ public class ProductService {
     public Optional<Product> getProductById(Long id) {
         return productRepository.findById(id);
     }
+
+    public Page<Product> getPaginatedList(int pageNo, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        return productRepository.findAll(pageable);
+    }
+
 }
